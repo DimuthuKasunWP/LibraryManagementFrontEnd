b9260bc6ab5bb614645c6b9dcc3bf00cb789a021
Dimuthu <kasunwpdimuthu@gmail.com>
2020-07-20 10:50:51 +0530

    project initialized

---
+++ src/main/java/lk/ac/cmb/ucsc/LibraryManagementFrontEnd/LibraryManagementFrontEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.cmb.ucsc.LibraryManagementFrontEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementFrontEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/cmb/ucsc/LibraryManagementFrontEnd/LibraryManagementFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.cmb.ucsc.LibraryManagementFrontEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
